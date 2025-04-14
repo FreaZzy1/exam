35772587b1e59f12d9c2b8ee9a93e22435941911
Subbotin.59 <egor.noskov05@mail.ru>
2025-04-14 09:06:07 +0500

    Добавьте файлы проекта.

---
+++ ViewModelMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class ViewModelMessage
+    {
+        List<string>Message = new List<string>();
+        public ViewModelMessage(List<string> message)
+        {
+            this.Message = message;
+        }
+
+    }
+}
